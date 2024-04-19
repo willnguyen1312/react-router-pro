552590f8d2ae30db20bc3eee01b3e67ed5f26662
Nam Nguyen <nam.nguyen@shopify.com>
2024-04-19 19:29:10 -0300

    Add msw 💞

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import { worker } from "./mocks/browser";
+
+worker.start().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,74 @@
+// src/mocks/handlers.js
+import { http, HttpResponse } from "msw";
+import { faker } from "@faker-js/faker";
+
+type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+};
+
+const users: User[] = Array.from({ length: 10 }, () => ({
+  id: faker.string.uuid(),
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+}));
+
+export const handlers = [
+  http.get("/users", () => {
+    return HttpResponse.json({
+      users,
+    });
+  }),
+
+  http.post("/users", async ({ request }) => {
+    const { firstName, lastName } = (await request.json()) as {
+      firstName: string;
+      lastName: string;
+    };
+
+    const newUser: User = {
+      id: faker.string.uuid(),
+      firstName,
+      lastName,
+    };
+
+    users.push(newUser);
+
+    return HttpResponse.json({
+      newUser,
+    });
+  }),
+
+  http.delete("/users/:id", async ({ params }) => {
+    const { id } = params;
+
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    users.splice(userIndex, 1);
+
+    return HttpResponse.json({
+      users,
+    });
+  }),
+
+  http.put("/users/:id", async ({ params, request }) => {
+    const { id } = params;
+    const { firstName, lastName } = (await request.json()) as {
+      firstName: string;
+      lastName: string;
+    };
+
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    users[userIndex] = {
+      ...users[userIndex],
+      firstName,
+      lastName,
+    };
+
+    return HttpResponse.json({
+      users,
+    });
+  }),
+];
